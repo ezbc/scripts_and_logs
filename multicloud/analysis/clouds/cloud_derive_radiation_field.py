e93a7e8b5c2dd809ea63a42e1c7cb6d3548a25cc
ezbc <ezbc@astro.wisc.edu>
2016-03-07 08:26:06 -0600

    init file for deriving radiation field image

---
+++ multicloud/analysis/clouds/cloud_derive_radiation_field.py
@@ -0,0 +1,24 @@
+
+#!/usr/bin/python
+
+import pickle
+import numpy as np
+from astropy.io import fits
+from astropy.coordinates import SkyCoord
+from astropy import units as u
+from astropy.io import fits
+from astropy.wcs import WCS
+import myimage_analysis as myia
+import mygeometry as myg
+import mystats
+
+
+
+
+def main():
+
+    pass
+
+if __name__ == '__main__':
+    main()
+
