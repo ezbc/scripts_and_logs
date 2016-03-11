b73ca832890df71cfa8e76b4081e8e98405a5c60
ezbc <ezbc@astro.wisc.edu>
2016-03-11 14:12:55 -0600

    added density cdfs

--- multicloud/analysis/models/multicloud_analysis_hi_temps.py
+++ multicloud/analysis/models/multicloud_analysis_hi_temps.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+
+import numpy as np
 
 def gaussian(x, amp, sigma, x0):
 
@@ -17,6 +20,30 @@ def read_data(filename, data_start=2, delimiter='\t'):
 
     return table
 
+def calc_avg_Ts(TB_dict, tau_dict, x,):
+
+    from scipy.integrate import simps
+
+    # initialize emtpy array
+    avg_Ts_array = np.empty(len(TB_dict))
+
+    # compute average temperature for each source
+    for i, source in enumerate(TB_dict):
+        TB = TB_dict[source]
+        tau = tau_dict[source]
+
+        mask_zero_val = (TB == 0) | (tau == 0)
+        TB_nonzero = TB[~mask_zero_val]
+        tau_nonzero = tau[~mask_zero_val]
+        x_nonzero = x[~mask_zero_val]
+
+        avg_Ts_array[i] = simps(TB_nonzero / (1.0 - np.exp(-tau_nonzero)),
+                                x_nonzero)
+        avg_Ts_array[i] = simps(TB_nonzero / tau_nonzero,
+                                x_nonzero)
+
+    return avg_Ts_array
+
 def main():
 
     import numpy as np
@@ -29,7 +56,7 @@ def main():
     clouds = ('taurus', 'perseus', 'california')
     dec_ranges = ((37, 19), (37, 19), (37, 35))
     ra_ranges = ((5.5, 3.8), (3.8, 2.9), (5.5, 3.5))
-    tspin_threshold = 10000.0
+    tspin_threshold = 500.0
 
     # Data locations in script
     # --------------------------------------------------------------------------
@@ -102,13 +129,17 @@ def main():
         # Get the spin temperatures of chosen sources
         t_cnm_list = []
         t_int_list = []
+        tk_list = []
+        tk_error_list = []
         t_cnm_error_list = []
         avg_tspin_list = []
         avg_tspin_error_list = []
         cloud_t_cnm_list = []
         source_list = []
         weights = []
-        avg_temp_dict = {}
+        TB_dict = {}
+        tau_dict = {}
+        velocities = np.linspace(-1000, 1000, 1000)
 
         for j in xrange(len(param_data)):
             tspin = param_data[j][param_cols.index('Ts')]
@@ -116,6 +147,13 @@ def main():
             tau = param_data[j][param_cols.index('tau')]
             tspin_error = param_data[j][param_cols.index('e_Ts')]
             source = param_data[j][param_cols.index('Source')]
+
+            Tpeak = param_data[j][param_cols.index('TB')]
+            Tk = param_data[j][param_cols.index('Tkmax')]
+            Tk_error = Tk * 0.1
+            DelV = param_data[j][param_cols.index('DelV')]
+            VLSR = param_data[j][param_cols.index('VLSR')]
+
             if source in cloud_sources:
                 if source not in source_list:
                     avg_temp_added = False
@@ -126,7 +164,15 @@ def main():
                 #print source
 
                 # add the source avg Tspin
-                if not avg_temp_added:
+                if 1:
+                #if not avg_temp_added:
+                    print ''
+                    print source
+                    print 'TB:', TB
+                    print 'tau:', tau
+                    print 'T_kinetic:', Tk
+                    print 'sigma:', DelV/2.0
+                    print 'vlsr:', VLSR
                     avg_temp = \
                         avg_temp_data[avg_temp_sources == source][0]
 
@@ -141,33 +187,26 @@ def main():
                     avg_tspin_error_list.append(avg_temp_error)
 
                 #avg_temp = np.mean(TB) / np.mean(tau)
-                if 0:
-                    print ''
-                    print source
-                    print TB
-                    print tau
-                    if source not in avg_temp_dict:
-                        avg_temp = TB / (1 - np.exp(-tau))
-                        avg_temp_dict[source] = avg_temp
+                else:
+
+                    # Calculate brightness temp and tau spectra
+                    TB_spectrum = gaussian(velocities, TB, DelV/2.0, VLSR)
+                    tau_spectrum = gaussian(velocities, tau, DelV/2.0, VLSR)
+
+                    # add them to the source list
+                    if source not in TB_dict:
+                        TB_dict[source] = TB_spectrum
+                        tau_dict[source] = tau_spectrum
                     else:
-                        avg_temp_dict[source] = \
-                            (avg_temp_dict[source] + TB / (1 - np.exp(-tau)))
-                    if TB < 0:
-                        print ''
-                        print source
-                        print 'tau =', tau
-                        print 'TB =', TB
-                        print 'Tavg =', avg_temp_dict[source]
+                        TB_dict[source] = \
+                            TB_dict[source] + TB_spectrum
+                        tau_dict[source] = \
+                            tau_dict[source] + tau_spectrum
 
                 #avg_tspin_list.append(avg_temp)
 
                 if tspin < tspin_threshold and tspin_error != 0.0:
                     cloud_t_cnm_list.append(tspin)
-
-                    Tpeak = param_data[j][param_cols.index('TB')]
-                    Tk = param_data[j][param_cols.index('Tkmax')]
-                    DelV = param_data[j][param_cols.index('DelV')]
-                    VLSR = param_data[j][param_cols.index('VLSR')]
 
                     T_integ = integrate(gaussian,
                                         -1000,
@@ -183,15 +222,25 @@ def main():
                         t_cnm_error_list.append(tspin_error)
                         t_int_list.append(Tk)
 
+
+                if VLSR < 15 and VLSR > -5:
+                    tk_list.append(Tk)
+                    tk_error_list.append(Tk_error)
+
+        # convert TB and tau to average spin temperatue
+        #avg_tspin_list = calc_avg_Ts(TB_dict, tau_dict, velocities)
+
+        print 'avg t spins:', avg_tspin_list
+
         temp_dict[cloud]['Ts_list'] = t_cnm_list
         temp_dict[cloud]['Ts_error_list'] = t_cnm_error_list
         #avg_tspin_list = []
-        #for source in avg_temp_dict:
-        #    avg_tspin_list.append(avg_temp_dict[source])
+        #for source in TB_dict:
+        #    avg_tspin_list.append(TB_dict[source])
         temp_dict[cloud]['Ts_avg_list'] = avg_tspin_list
         temp_dict[cloud]['Ts_avg_error_list'] = avg_tspin_error_list
-
-        print avg_tspin_list
+        temp_dict[cloud]['Tk_list'] = tk_list
+        temp_dict[cloud]['Tk_error_list'] = tk_error_list
 
         if 0:
             if cloud=='perseus':
