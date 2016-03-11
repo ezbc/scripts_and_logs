887a5868629c0d2e867526bad82c538a7e264840
ezbc <ezbc@astro.wisc.edu>
2016-03-11 14:09:16 -0600

    ?

--- multicloud/analysis/clouds/cloud_derive_radiation_field.py
+++ multicloud/analysis/clouds/cloud_derive_radiation_field.py
@@ -13,11 +13,48 @@ import mygeometry as myg
 import mystats
 
 
+def write_rad_field():
 
+    from myscience import calc_radiation_field
+
+    # Define filenames
+    # ------------
+    DIR_DUST = '/d/bip3/ezbc/multicloud/data/dust_temp/'
+    DIR_RAD = '/d/bip3/ezbc/multicloud/data/radiation_field/'
+    FILENAME_TEMPERATURE = DIR_DUST + 'multicloud_dust_temp_5arcmin.fits'
+    FILENAME_TEMPERATURE_ERROR = DIR_DUST + \
+            'multicloud_dust_temp_error_5arcmin.fits'
+    FILENAME_BETA = DIR_DUST + 'multicloud_dust_beta_5arcmin.fits'
+    FILENAME_BETA_ERROR = DIR_DUST + 'multicloud_dust_beta_error_5arcmin.fits'
+    FILENAME_RAD = DIR_RAD + 'multicloud_rad_field_5arcmin.fits'
+    FILENAME_RAD_ERROR = DIR_RAD + 'multicloud_rad_field_error_5arcmin.fits'
+
+    # Get the data
+    # ------------
+    temp_data, temp_header = fits.getdata(FILENAME_TEMPERATURE, header=True)
+    temp_error_data = fits.getdata(FILENAME_TEMPERATURE_ERROR)
+    beta_data, beta_header = fits.getdata(FILENAME_BETA, header=True)
+    beta_error_data = fits.getdata(FILENAME_BETA_ERROR)
+
+    # Calculate the radiation field
+    # -----------------------------
+    rad_field, rad_field_error = calc_radiation_field(temp_data,
+                                                      T_dust_error=temp_error_data,
+                                                      beta=beta_data,
+                                                      beta_error=beta_error_data)
+
+    # create rad field header
+    rad_field_header = beta_header.copy()
+    rad_field_header['BUNIT'] = 'Mathis'
+
+    # Write the radiation field to fits file
+    fits.writeto(FILENAME_RAD, rad_field, header=rad_field_header, clobber=True)
+    fits.writeto(FILENAME_RAD_ERROR, rad_field_error, header=rad_field_header,
+                 clobber=True,)
 
 def main():
 
-    pass
+    write_rad_field()
 
 if __name__ == '__main__':
     main()
