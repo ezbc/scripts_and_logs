0d16d876272ebdf6943cb42dafc1769f068a86ad
ezbc <ezbc@astro.wisc.edu>
2016-03-30 14:25:41 -0500

    successful plotting of nhi and nh2

--- multicloud/analysis/av/multicloud_plot_nh2_maps.py
+++ multicloud/analysis/av/multicloud_plot_nh2_maps.py
@@ -171,6 +171,163 @@ def plot_h2_sd(plot_dict, contour_image=None, limits=None,
     if show:
         fig.show()
 
+def plot_hi_h2_sd(plot_dict, contour_image=None, limits=None, filename=None,
+        show=True, hi_vlimits=None, h2_vlimits=None, av_vlimits=None,
+        cloud_names=('california', 'perseus', 'taurus'),):
+
+    # Import external modules
+    import matplotlib.pyplot as plt
+    import matplotlib
+    import numpy as np
+    import pyfits as fits
+    import matplotlib.pyplot as plt
+    import myplotting as myplt
+    import pywcsgrid2
+    import pywcs
+    from pylab import cm # colormaps
+    from matplotlib.patches import Polygon
+    from mpl_toolkits.axes_grid1.axes_grid import AxesGrid
+
+    # Set up plot aesthetics
+    plt.clf(); plt.close()
+
+    # Colormap settings
+    cmap = plt.cm.copper
+    cmap.set_bad(color='w')
+    norm = matplotlib.colors.LogNorm()
+
+    # Create figure instance
+    figsize = (7.5, 9)
+    fig = pywcsgrid2.plt.figure(figsize=figsize)
+
+    colorbar_axes = [0.05, 0.97, 0.95, 0.02,]
+    map_axes = np.array([0.05, 0.69, 0.95, 0.25])
+    map_axes = np.array([0.05, 0.31, 0.95, 0.25])
+
+    # plot HI and H2 maps for each cloud
+    # ---------------------------------------------------------------------------
+    for i, cloud_name in enumerate(cloud_names):
+        # get image properties
+        # -----------------------------------------------------------------------
+        header = plot_dict[cloud_name]['header']
+        limits = plot_dict[cloud_name]['limits']
+        contour_image = plot_dict[cloud_name]['contour_image']
+        contours = plot_dict[cloud_name]['contours']
+
+        # Prep axes instance
+        # -----------------------------------------------------------------------
+        nrows_ncols=(1, 2)
+        ngrids=2
+
+        # only show cbar for first row
+        if i == 0:
+            cbar_mode = "each"
+        else:
+            cbar_mode = None
+
+        axes = AxesGrid(fig, 311 + i, #(3,1,i),
+                     nrows_ncols=nrows_ncols,
+                     ngrids=ngrids,
+                     cbar_mode=cbar_mode,
+                     cbar_location='top',
+                     cbar_pad="5%",
+                     cbar_size='10%',
+                     axes_pad=0.1,
+                     axes_class=(pywcsgrid2.Axes,
+                                 dict(header=header)),
+                     aspect=True,
+                     label_mode='L',
+                     share_all=True,
+                     )
+
+        # get axes from row
+        ax1 = axes[0]
+        ax2 = axes[1]
+
+        # Plot the maps
+        # -----------------------------------------------------------------------
+        # HI surface density map
+        hi_sd = plot_dict[cloud_name]['hi_sd']
+
+        # H2 surface density map
+        h2_sd = plot_dict[cloud_name]['h2_sd']
+
+        # show the images
+        im_hi = ax1.imshow(hi_sd,
+                interpolation='nearest',
+                origin='lower',
+                cmap=cmap,
+                vmin=hi_vlimits[0],
+                vmax=hi_vlimits[1],
+                #norm=matplotlib.colors.LogNorm()
+                )
+        im_h2 = ax2.imshow(h2_sd,
+                interpolation='nearest',
+                origin='lower',
+                cmap=cmap,
+                vmin=h2_vlimits[0],
+                vmax=h2_vlimits[1],
+                #norm=matplotlib.colors.LogNorm()
+                )
+
+        # Asthetics
+        # -----------------------------------------------------------------------
+        for ax in (ax1, ax2):
+            ax.set_display_coord_system("fk5")
+            ax.set_ticklabel_type("hms", "dms")
+            ax.set_xlabel('Right Ascension [J2000]',)
+            ax.set_ylabel('Declination [J2000]',)
+            ax.locator_params(nbins=5)
+
+        # colorbar
+        # -----------------------------------------------------------------------
+        cb_hi = ax1.cax.colorbar(im_hi)
+        cb_h2 = ax2.cax.colorbar(im_h2)
+
+        # Write label to colorbar
+        cb_hi.set_label_text(r'$\Sigma_{\rm HI}$ [M$_\odot$\,pc$^{-2}$]',)
+        cb_h2.set_label_text(r'$\Sigma_{\rm H_2}$ [M$_\odot$\,pc$^{-2}$]',)
+
+        # plot limits
+        # -----------------------------------------------------------------------
+        if limits is not None:
+            limits_pix = myplt.convert_wcs_limits(limits, header, frame='fk5')
+            ax1.set_xlim(limits_pix[0],limits_pix[1])
+            ax1.set_ylim(limits_pix[2],limits_pix[3])
+            ax2.set_xlim(limits_pix[0],limits_pix[1])
+            ax2.set_ylim(limits_pix[2],limits_pix[3])
+
+        # Plot Av contours
+        # -----------------------------------------------------------------------
+        if contour_image is not None:
+            ax1.contour(contour_image, levels=contours, colors='w')
+            ax2.contour(contour_image, levels=contours, colors='w')
+
+        # Cloud names
+        # -----------------------------------------------------------------------
+        ax1.annotate(cloud_name.capitalize(),
+                    xytext=(0.96, 0.94),
+                    xy=(0.96, 0.94),
+                    textcoords='axes fraction',
+                    xycoords='axes fraction',
+                    size=10,
+                    color='k',
+                    bbox=dict(boxstyle='square',
+                              facecolor='w',
+                              alpha=1),
+                    horizontalalignment='right',
+                    verticalalignment='top',
+                    )
+
+        # create new box for plot
+        # -----------------------------------------------------------------------
+        #map_axes[1] -= 0.3
+        map_axes[1] += 0.3
+
+    if filename is not None:
+        plt.savefig(filename, bbox_inches='tight')
+    if show:
+        fig.show()
 
 '''
 The main script
@@ -206,14 +363,17 @@ def load_results(filename, load_fits=True):
 def main():
 
     # define constants
+    # ---------------------------------------------------------------------------
     DIR_FIGURES = '/d/bip3/ezbc/multicloud/figures/'
     DIR_RESULTS = '/d/bip3/ezbc/multicloud/data/python_output/bootstrap_results/'
     FILENAME_EXT = '_planck_noint_gaussrange_isotropic_bootstrap_results.pickle'
-    FILENAME_PLOT_BASE = DIR_FIGURES + 'maps/h2_maps'
+    FILENAME_PLOT_BASE_H2 = DIR_FIGURES + 'maps/h2_maps'
+    FILENAME_PLOT_BASE = DIR_FIGURES + 'maps/hi_h2_maps'
     PLOT_FILETYPES = ['png', 'pdf']
     CLOUD_NAMES = ['california', 'perseus', 'taurus']
 
     # get the data needed to plot
+    # ---------------------------------------------------------------------------
     plot_dict = {}
     for cloud_name in CLOUD_NAMES:
         plot_dict[cloud_name] = {}
@@ -222,28 +382,38 @@ def main():
         # load the analysis
         results = load_results(DIR_RESULTS + cloud_name + FILENAME_EXT)
 
+        # Write data to dictionary for the plotting function
+        # -----------------------------------------------------------------------
         hi_sd = results['data_products']['hi_sd']
         h2_sd = results['data_products']['h2_sd']
 
         cloud_dict['contour_image'] = None# hi_sd
         cloud_dict['contours'] = [4, 8]
+        cloud_dict['hi_sd'] = hi_sd
         cloud_dict['h2_sd'] = h2_sd
         cloud_dict['header'] = results['data']['av_header']
 
+        # set cloud limits
+        # -----------------------------------------------------------------------
         if cloud_name == 'california':
-            plot_dict[cloud_name]['limits'] = [75, 60, 30, 38]
+            plot_dict[cloud_name]['limits'] = [74, 60, 30, 38]
         if cloud_name == 'perseus':
-            plot_dict[cloud_name]['limits'] = [61, 45, 24, 36]
+            plot_dict[cloud_name]['limits'] = [61, 46, 24, 36]
         if cloud_name == 'taurus':
-            plot_dict[cloud_name]['limits'] = [75, 57, 20, 33]
+            plot_dict[cloud_name]['limits'] = [74, 57, 20, 33]
 
     # plot the 3-panel H2 surface density map
+    # ---------------------------------------------------------------------------
     for filetype in PLOT_FILETYPES:
+        plot_hi_h2_sd(plot_dict,
+                      filename=FILENAME_PLOT_BASE + '.' + filetype,
+                      hi_vlimits=[5, 13],
+                      h2_vlimits=[-0.1, 50],
+                      )
         plot_h2_sd(plot_dict,
-                   filename=FILENAME_PLOT_BASE + '.' + filetype,
+                   filename=FILENAME_PLOT_BASE_H2 + '.' + filetype,
                    vlimits=[-0.1, 50],
                    )
-
 
 if __name__ == '__main__':
     main()
